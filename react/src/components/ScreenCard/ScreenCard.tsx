414edd30a0c186e40ebc17f1160b110c3a635cd8
Renan Pamplona <renan@clientbase.com.br>
2025-04-25 14:09:33 -0300

    chore: add name to the card

--- react/src/components/ScreenCard/ScreenCard.tsx
+++ react/src/components/ScreenCard/ScreenCard.tsx
@@ -3,8 +3,12 @@ import React from "react";
 const ScreenCard = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="min-h-screen max-h-screen bg-slate-200 grid place-items-center">
-      <div className="bg-white p-8 rounded-md flex flex-col gap-6 min-w-xl my-4 max-h-[90vh]">
+      <div className="relative bg-white p-8 rounded-md flex flex-col gap-6 min-w-xl my-4 max-h-[90vh]">
         {children}
+
+        <span className="text-xs text-muted-foreground absolute right-1 bottom-1">
+          by Renan Pamplona
+        </span>
       </div>
     </div>
   );
